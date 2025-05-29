70a4fa867314b70666d280df14074914663fd9e6
moghe <mogheshwetha15@gmail.com>
2025-05-29 21:59:47 +0530

    ALTER, DROP commands

--- SQL/Scripts/DDL.sql
+++ SQL/Scripts/DDL.sql
@@ -1,9 +1,19 @@
 --Create table
-CREATE TABLE
-  persons (
-    id INT NOT NULL,
-    person_name VARCHAR(50) NOT NULL,
-    birth_date DATE,
-    phone VARCHAR(15) NOT NULL,
-    CONSTRAINT pk_persons PRIMARY KEY (id)
-  )
+CREATE TABLE persons (
+  id INT NOT NULL,
+  person_name VARCHAR(50) NOT NULL,
+  birth_date DATE,
+  phone VARCHAR(15) NOT NULL,
+  CONSTRAINT pk_persons PRIMARY KEY (id)
+) --Add a column
+ALTER TABLE
+  persons
+ADD
+  email VARCHAR(50) NOT NULL;
+
+--Remove column
+ALTER TABLE
+  persons DROP COLUMN email;
+
+--Drop table
+DROP TABLE persons;
