48b2eeea281ca88c15c2cf38283bc6e4f82c90f0
moghe <mogheshwetha15@gmail.com>
2025-06-02 19:59:51 +0530

    Section 5: NOT,BETWEEN,LIKE,IN perators

--- SQL/Scripts/Filter.sql
+++ SQL/Scripts/Filter.sql
@@ -59,3 +59,63 @@ FROM
 WHERE
   country = 'USA'
   OR score > 500;
+
+--NOT
+SELECT
+  *
+FROM
+  customers
+WHERE
+  NOT score >= 500;
+
+SELECT
+  *
+FROM
+  customers
+WHERE
+  score IS NOT NULL;
+
+--BETWEEN
+SELECT
+  *
+FROM
+  customers
+WHERE
+  score BETWEEN 100 AND 500;
+
+--IN
+SELECT
+  *
+FROM
+  customers
+WHERE
+  country IN ('USA', 'Germany');
+
+SELECT
+  *
+FROM
+  customers
+WHERE
+  country NOT IN ('USA', 'Germany');
+
+--LIKE
+SELECT
+  *
+FROM
+  customers
+WHERE
+  first_name LIKE 'M%';
+
+SELECT
+  *
+FROM
+  customers
+WHERE
+  first_name LIKE '%r%';
+
+SELECT
+  *
+FROM
+  customers
+WHERE
+  first_name LIKE '__r%';
