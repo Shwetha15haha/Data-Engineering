d32073a172638a7dcdbb78299fd8ded7a06d1890
moghe <mogheshwetha15@gmail.com>
2025-06-08 10:52:58 +0530

    ANTI joins, Cross join

--- SQL/Scripts/JOINS.sql
+++ SQL/Scripts/JOINS.sql
@@ -70,3 +70,88 @@ SELECT
 FROM
   customers c
   FULL JOIN orders o ON c.id = o.customer_id;
+
+--LEFT ANTI JOIN
+--Returns rows from left table that has no match in right table
+--Use case: Fetch list of customers who didn't order
+SELECT
+  *
+FROM
+  MyDatabase.dbo.customers c
+  LEFT JOIN MyDatabase.dbo.orders o ON c.id = o.customer_id
+WHERE
+  o.customer_id IS NULL;
+
+--RIGHT ANTI JOIN
+--Returns rows from right table that has no match in left table
+--Use case: get orders without matching customers
+SELECT
+  *
+FROM
+  MyDatabase.dbo.customers c
+  RIGHT JOIN MyDatabase.dbo.orders o ON c.id = o.customer_id
+WHERE
+  c.id IS NULL;
+
+--OR
+SELECT
+  *
+FROM
+  MyDatabase.dbo.orders o
+  LEFT JOIN MyDatabase.dbo.customers c ON c.id = o.customer_id
+WHERE
+  c.id IS NULL;
+
+--FULL ANTI JOIN
+--Opposite of inner join
+--Returns data thet doen't match in either tables
+--Use case : Find customers without orders and orders without customers
+SELECT
+  *
+FROM
+  MyDatabase.dbo.customers c
+  FULL JOIN MyDatabase.dbo.orders o ON c.id = o.customer_id
+WHERE
+  c.id IS NULL
+  OR o.customer_id IS NULL;
+
+--Get all customers along with orders , but only for customers who have order Without using Inner join
+SELECT
+  *
+FROM
+  MyDatabase.dbo.customers c
+  FULL JOIN MyDatabase.dbo.orders o ON c.id = o.customer_id
+WHERE
+  o.customer_id IS NOT NULL;
+
+SELECT
+  *
+FROM
+  MyDatabase.dbo.customers c
+  LEFT JOIN MyDatabase.dbo.orders o ON c.id = o.customer_id
+WHERE
+  o.customer_id IS NOT NULL;
+
+SELECT
+  *
+FROM
+  MyDatabase.dbo.customers c
+  CROSS JOIN MyDatabase.dbo.orders o;
+
+--CROSS JOIN
+--Each row from right table combines with each row in left table
+--2 rows in A, 3 rows in B produces 6 rows output. Here it doesn't matter whether it is matching or not. There is no matching column to specifiy.
+-- Imagine students in a school and sports they play.
+-- - FULL JOIN: Matches students with their sports. If a student doesn’t play a sport, their name appears with NULL. If a sport has no players, it still appears with NULL.
+-- - CROSS JOIN: Pairs every student with every sport, even if they don’t actually play that sport—just listing all possible combinations!
+-- CROSS JOIN = One - to - Many (All Pairings)
+-- FULL JOIN = One - to - One
+-- or One - to - None (
+--   Matching
+--   where
+--     possible,
+--     NULLs
+--   where
+--     missing
+-- )
+--MULTIPLE  TABLE JOINS
