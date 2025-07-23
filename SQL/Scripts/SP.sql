8ff0fb1d802b25efd0b7abdac14678180755b0c8
moghe <mogheshwetha15@gmail.com>
2025-07-23 20:22:46 +0530

    parameters and defaults

--- SQL/Scripts/SP.sql
+++ SQL/Scripts/SP.sql
@@ -7,7 +7,6 @@ FROM
   SalesDB.Sales.customers
 WHERE
   Country = 'USA';
-
 -- step 2: create a stored procedure
 CREATE PROCEDURE GetUSCustomerStats AS BEGIN
 SELECT
@@ -17,6 +16,31 @@ FROM
   SalesDB.Sales.customers
 WHERE
   Country = 'USA' END;
-
 -- step 3: execute the stored procedure
 EXEC GetUSCustomerStats;
+--Parameters can be added to the stored procedure if needed
+-- Example with parameters
+CREATE PROCEDURE GetCustomerStatsByCountry @Country NVARCHAR(50)
+AS BEGIN
+SELECT
+  COUNT(CustomerID) AS total_customers,
+  AVG(Score) AS avgscore
+FROM
+  SalesDB.Sales.customers
+WHERE
+  Country = @Country END;
+EXEC GetCustomerStatsByCountry @Country='USA';
+EXEC GetCustomerStatsByCountry @Country='Germany';
+
+-- step 4: add default values to parameters
+ALTER PROCEDURE GetCustomerStatsByCountry @Country NVARCHAR(50) = 'USA'
+AS BEGIN
+SELECT
+  COUNT(CustomerID) AS total_customers,
+  AVG(Score) AS avgscore
+FROM
+  SalesDB.Sales.customers
+WHERE
+  Country = @Country END;
+
+EXEC GetCustomerStatsByCountry ;
